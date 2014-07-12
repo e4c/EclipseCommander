26293decdb16148de2c152e882a5a88b13c21ed7
webcane <webcane@ya.ru>
2014-07-12 17:35:58 +0300

    add java 7 File API example

--- cane.brothers.e4.efs.test2/src/cane/brothers/e4/efs/test2/parts/NIO2Part.java
+++ cane.brothers.e4.efs.test2/src/cane/brothers/e4/efs/test2/parts/NIO2Part.java
@@ -1,7 +1,16 @@
 package cane.brothers.e4.efs.test2.parts;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.jface.viewers.TableViewer;
@@ -18,11 +27,6 @@ public class NIO2Part {
 	private Text txtInput;
 	private TableViewer tableViewer;
 
-	@Inject
-	public NIO2Part() {
-		// TODO Your code here
-	}
-
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
@@ -35,8 +39,52 @@ public class NIO2Part {
 
 				if (e.getSource() == txtInput) {
 					Text txt = (Text) e.getSource();
+					Path path = null;
+					try {
+						path = Paths.get(txt.getText());
+					} catch (InvalidPathException ex) {
+						System.out.println(ex.getMessage());
+						path = Paths.get("/");
+					}
 
+					tableViewer.getTable().removeAll();
+
+					for (String str : createInput(path)) {
+						tableViewer.add(str);
+					}
+					tableViewer.getControl().update();
 				}
+			}
+
+			private List<String> createInput(Path path) {
+				List<String> list = new ArrayList<>();
+
+				if (path != null) {
+					list.add("Path.fileName : " + path.getFileName());
+					list.add("Path.fullName : " + path.toString());
+					list.add("Path.fileSystem.separator : "
+							+ path.getFileSystem().getSeparator());
+					list.add("Path.root : " + path.getRoot());
+					list.add("is Dir: " + Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS));
+					list.add("is Absolute: " + path.isAbsolute());
+
+					if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+						list.add("names:");
+						DirectoryStream<Path> dir = null;
+						try {
+							dir = Files.newDirectoryStream(path);
+						} catch (IOException e) {
+							System.out.println(e.getMessage());
+						}
+
+						if (dir != null) {
+							for (Path filename : dir) {
+								System.out.println(filename);
+							}
+						}
+					}
+				}
+				return list;
 			}
 
 		});
