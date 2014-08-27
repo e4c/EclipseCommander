dbfda95a79e6fb6ece02acafeaaa5426860f8166
cane <webcane@ya.ru>
2014-08-27 23:47:36 +0400

    rename events class

--- cane.brothers.e4.commander/src/cane/brothers/e4/commander/PathEvents.java
+++ cane.brothers.e4.commander/src/cane/brothers/e4/commander/PathEvents.java
@@ -17,11 +17,30 @@
 package cane.brothers.e4.commander;
 
 /**
- * TODO
+ * E4 UI path event constants.
  *
  */
-public final class MyEventConstants {
+public final class PathEvents {
 
-    public static final String TAB_PATH_OPEN = "TAB_PATH/OPEN";
+    /**
+     * Base name of all app events
+     */
+    public static final String TOPIC_BASE = "cane/brothers/e4/commander/ui"; //$NON-NLS-1$
+
+    /**
+     * Base name of all path events
+     */
+    public static final String PATH_TOPIC_BASE = TOPIC_BASE + "/tab/path/"; //$NON-NLS-1$
+
+    /**
+     * Sent when opening new path in active tab.
+     */
+    public static final String TAB_PATH_OPEN = PATH_TOPIC_BASE + "open"; //$NON-NLS-1$
+
+    /**
+     * Sent when necessary to remove selection from opposite tab.
+     */
+    public static final String TAB_REMOVE_SELECTION = PATH_TOPIC_BASE
+	    + "selection/remove"; //$NON-NLS-1$
 
 }
