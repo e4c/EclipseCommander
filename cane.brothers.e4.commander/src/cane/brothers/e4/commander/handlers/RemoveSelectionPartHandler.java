4a27d0bc2344403ad86cc89990d380f6e0712e4d
cane <webcane@ya.ru>
2014-08-28 23:59:48 +0400

    cleanup

--- cane.brothers.e4.commander/src/cane/brothers/e4/commander/handlers/RemoveSelectionPartHandler.java
+++ cane.brothers.e4.commander/src/cane/brothers/e4/commander/handlers/RemoveSelectionPartHandler.java
@@ -21,9 +21,7 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.UIEventTopic;
-import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 import cane.brothers.e4.commander.PathEvents;
@@ -35,18 +33,12 @@ import cane.brothers.e4.commander.utils.PartUtils;
 public class RemoveSelectionPartHandler {
 
     @Inject
-    MApplication application;
-
-    @Inject
-    EModelService modelService;
-
-    @Inject
     private EPartService partService;
 
     @Inject
     @Optional
     @Execute
-    public void execute(
+    public void removeSelection(
 	    @UIEventTopic(PathEvents.TAB_REMOVE_SELECTION) MPart activePart) {
 
 	if (activePart != null && partService != null) {
